3c3debdccef11dfbc536809b7dbb285d93665ebf
DAVID_RODRIGUEZ_JURADO <a.21308051280543@cbtis128.edu.mx>
2024-04-26 17:28:14 +0000

    version2

--- lib/main.dart
+++ lib/main.dart
@@ -9,34 +9,175 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Act6 Tablas Ford ',
+      home: MyHomePage(title: 'Tablas Ford'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: PageView(
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'images/fordgtenfrente.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text("Ford shelby 500.",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                              )),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.arrow_back),
+                          Text("Texto")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ), //carta1
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'images/fordraptorenfrente.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text("Ford shelby 350",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                              )),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.arrow_circle_right),
+                          Text("Texto")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ), //carta2
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'images/fordshelby500enfrente.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text("Ford shelby 350",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                              )),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.arrow_back),
+                          Text("Texto")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ), //carta3
+          Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'images/MustangEfrente.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const Center(
+                child: SizedBox(
+                  width: 150,
+                  height: 200,
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Text("Ford shelby 350",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                              )),
+                          Divider(
+                            thickness: 2,
+                            height: 25,
+                          ),
+                          Icon(Icons.arrow_circle_right),
+                          Text("Texto")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ), //carta4
+        ],
       ),
     );
   }
